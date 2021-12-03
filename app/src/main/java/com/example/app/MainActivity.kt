9253934ff9c9c1a703be30624f5b5bb139e73cae
Jesufertez <maria.fernandez.cortez@gmail.com>
2021-12-02 23:09:41 -0300

    Se agrega Bottom Navigation Bar, se cambian colores de los botones y el fondo del bottomm, se enchula el layout de personajes

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var rawBindind: ActivityMainBinding? = null
+    private val binding get() = rawBindind!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        rawBindind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.content) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        rawBindind = null
     }
 }
